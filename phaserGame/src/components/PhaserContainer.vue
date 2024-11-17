43a5e743e52ef827fef0e0fd7f537977abce5e3b
Alejandro <159506396+YahirArboleya@users.noreply.github.com>
2024-11-17 00:14:52 +0000

    ☠️fail no1: no encuentra el componenete

---
+++ phaserGame/src/components/PhaserContainer.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="game">
+        <ion-button v-if="showButton" @click="handleClickStart">
+            Start Game
+        </ion-button>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from "vue";
+    import { IonButton } from "@ionic/vue";
+    import { Game } from "phaser";
+
+    const showButton = ref(true)
+
+    function launch(){
+
+    }
+
+    function handleClickStart() {
+        showButton.value = false;
+        launch();
+    }
+
+</script>
+
+<style scoped>
+
+    #game {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    }
+
+</style>
