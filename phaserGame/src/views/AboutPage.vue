60471c702dfda98a2475a14cb676ad7b82afd297
aaronalonso <aaronalonso200216@gmail.com>
2024-11-22 22:23:49 -0600

    👌FEAT: Add Game objects and Adjusting Game Window

--- phaserGame/src/views/AboutPage.vue
+++ phaserGame/src/views/AboutPage.vue
@@ -5,16 +5,10 @@
         <ion-title>Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
-    </ion-content>
-  </ion-page>
+<ion-content>
+  <ExploreContainer name="Tab 1 page" />
+</ion-content>
+</ion-page>
 </template>
 
 <script setup lang="ts">
