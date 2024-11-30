9e8c42203c99d4c97c6e6f5baf9e263a0e491e9d
aaronalonso <aaronalonso200216@gmail.com>
2024-11-29 19:19:34 -0600

    ❤️‍🔥Feat: solving the error

--- phaserGame/src/game/game.js
+++ phaserGame/src/game/game.js
@@ -2,21 +2,22 @@ import { Game, AUTO, Scale } from "phaser";
 import { PlayScene } from "./PlayScene.js";
 import { ScoreScene } from "./ScoreScene.js";
 
-export function launch() {
+
+  export function launch() {
     return new Game({
       type: AUTO,
       scale: {
-        mode: Scale.RESIZE,
-        width: window.innerWidth * window.devicePixelRatio,
+        mode: Scale.FIT, // O Scale.NONE si prefieres un tamaño fijo
+        width: window.innerWidth,
+        height: window.innerHeight,
         autoCenter: Scale.CENTER_BOTH,
-        height: window.innerHeight * window.devicePixelRatio,
       },
       parent: "game",
       backgroundColor: "#201726",
       physics: {
         default: "arcade",
       },
-      scene: [PlayScene,ScoreScene]
+      scene: [PlayScene, ScoreScene],
     });
   }
   
