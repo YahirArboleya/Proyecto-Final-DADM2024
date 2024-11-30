a0920eebf744a1dc94bdac86a8596fc74497b858
aaronalonso <aaronalonso200216@gmail.com>
2024-11-29 19:35:41 -0600

    🚧FEAT: Correccion

--- phaserGame/src/game/game.js
+++ phaserGame/src/game/game.js
@@ -3,21 +3,21 @@ import { PlayScene } from "./PlayScene.js";
 import { ScoreScene } from "./ScoreScene.js";
 
 
-  export function launch() {
-    return new Game({
-      type: AUTO,
-      scale: {
-        mode: Scale.FIT, // O Scale.NONE si prefieres un tamaño fijo
-        width: window.innerWidth,
-        height: window.innerHeight,
-        autoCenter: Scale.CENTER_BOTH,
-      },
-      parent: "game",
-      backgroundColor: "#201726",
-      physics: {
-        default: "arcade",
-      },
-      scene: [PlayScene, ScoreScene],
-    });
-  }
+export function launch() {
+  return new Game({
+    type: AUTO,
+    scale: {
+      mode: Scale.FIT, // O Scale.NONE si prefieres un tamaño fijo
+      width: window.innerWidth,
+      height: window.innerHeight,
+      autoCenter: Scale.CENTER_BOTH,
+    },
+    parent: "game",
+    backgroundColor: "#201726",
+    physics: {
+      default: "arcade",
+    },
+    scene: [PlayScene, ScoreScene],
+  });
+}
   
